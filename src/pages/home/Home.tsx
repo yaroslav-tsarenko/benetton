39ff5265ea48176f5f9bd7f63af1951a18d5633b
Yaroslav <yaroslav7v@gmail.com>
2024-08-23 19:54:27 +0300

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Home.module.scss";
+
+const Home = () => {
+    return (
+        <>
+            I'm home page
+        </>
+    );
+};
+
+export default Home;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <>
+         i'm login page
+        </>
+    );
+};
+
+export default Login;
