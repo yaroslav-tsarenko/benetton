a85a3e13822e3086e7031e6998928aeb30a99bb7
Yaroslav <yaroslav7v@gmail.com>
2024-08-23 19:55:46 +0300

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import AdminPanel from "./pages/admin-panel/AdminPanel";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/admin-panel" element={<AdminPanel />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/admin-panel/AdminPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./AdminPanel.module.scss";
+
+const AdminPanel = () => {
+    return (
+        <div>
+            i'm admin panel
+        </div>
+    );
+};
+
+export default AdminPanel;
